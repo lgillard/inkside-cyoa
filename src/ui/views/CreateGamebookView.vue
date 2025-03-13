592791fe83e984026ffb39d6db73015e04cee68e
Lucille Gillard <gillard.lucille@gmail.com>
2025-03-13 15:51:16 +0100

    ADD: Legend with labels to improve accessibility

--- src/ui/views/CreateGamebookView.vue
+++ src/ui/views/CreateGamebookView.vue
@@ -3,21 +3,43 @@
     <h2 class="text-primary text-h4 my-4">Écrire un nouveau livre</h2>
 
     <v-form class="centering-row">
-          <div id="book">
-            <FlatBook id="background"/>
-            <textarea
-              id="title-input"
-              placeholder="Titre du livre"/>
-            <input
-              id="author-name-input"
-              placeholder="Nom de l'auteur"
-              type="text"/>
-          </div>
+      <!-- Legend column part containing labels -->
+      <div id="legend-column" class="text-h5 d-flex flex-column justify-space-between">
+        <div><label for="title-input">Titre du livre</label> :</div>
+        <div class="d-flex">
+          <div><label for="cover-color-input">Couleur de la couverture</label> :</div>
+          <ColorPicker
+            id="cover-color-input"
+            v-model="coverColor"
+            type="color"/>
+        </div>
+        <div><label for="author-name-input">Nom de l'auteur</label> :</div>
+      </div>
+
+      <!-- Book part containing inputs-->
+      <div id="book">
+        <FlatBook id="background" :color="coverColor"/>
+        <textarea
+          id="title-input"
+          placeholder="Titre du livre"
+          class="text-h4"/>
+        <input
+          id="author-name-input"
+          placeholder="Nom de l'auteur"
+          type="text"
+          class="text-h5"/>
+      </div>
+
+      <!-- Pour centrer sur le livre et non sur sa légende -->
+      <div class="col-spacer"></div>
     </v-form>
   </div>
 </template>
 <script lang="ts" setup>
   import FlatBook from "@/ui/components/icons/FlatBook.vue";
+  import ColorPicker from "@/ui/components/ColorPicker.vue";
+
+  const coverColor = defineModel({ default: '#8b0000'});
 </script>
 <style lang="scss">
 .centering-row {
@@ -27,38 +49,64 @@
 
 #book {
   position: relative;
+  flex: 0 1 auto;
+  margin-right: 15vw;
+
+  #background {
+    max-height: 65vh;
+    max-width: 65vw;
+    height: 65vh;
+  }
+
+  #title-input {
+    position: absolute;
+    text-align: center;
+    left: 0;
+    padding: 5%;
+    margin-left: 6%;
+    width: 94%;
+    height: 40%;
+    resize: none;
+    overflow: hidden;
+    word-wrap: break-word;
+    white-space: pre-wrap;
+  }
+
+  #author-name-input {
+    position: absolute;
+    text-align: end;
+    right: 0;
+    bottom: 0;
+    margin-bottom: 12%;
+    padding: 5%;
+    width: 94%;
+    height: 15%;
+    font-size: 1.5rem;
+  }
 }
 
-#background {
-  max-height: 65vh;
-  max-width: 65vw;
-  height: 65vh;
-}
-
-#title-input {
-  position: absolute;
-  text-align: center;
-  left: 0;
-  padding: 5%;
-  margin-left: 6%;
-  width: 94%;
-  height: 40%;
-  font-size: 2rem;
-  resize: none;
-  overflow: hidden;
-  word-wrap: break-word;
-  white-space: pre-wrap;
-}
-
-#author-name-input {
-  position: absolute;
+#legend-column {
+  flex: 1 1 auto;
   text-align: end;
-  right: 0;
-  bottom: 0;
-  margin-bottom: 12%;
-  padding: 5%;
-  width: 70%;
-  height: 15%;
-  font-size: 1.5rem;
+  padding-right: 5px;
+  padding-top: 2%;
+  padding-bottom: 5%;
+
+  #cover-color-input {
+    width: 40px;
+    height: 40px;
+    margin-top: auto;
+  }
+
+  div:has(> label[for='cover-color-input']) {
+    padding-top: 10px;
+    padding-right: 10px;
+    margin-left: auto;
+  }
+}
+
+.col-spacer {
+  flex: 1 1 100px;
+  min-width: 0;
 }
 </style>
