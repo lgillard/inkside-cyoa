c253f1bf6cc115619ba9dda25e5265cdf4bf733f
Lucille Gillard <gillard.lucille@gmail.com>
2025-03-13 14:17:41 +0100

    ADD: color picker componant

---
+++ src/ui/components/ColorPicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+      :value="props.modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
+      type="color"
+  />
+</template>
+
+<script setup lang="ts">
+  const props = defineProps(['modelValue'])
+  const emit = defineEmits(['update:modelValue'])
+</script>
+
+<style scoped lang="scss">
+input[type="color" i] {
+  border-radius: 50%;
+  border: none;
+}
+
+/* Affects the inner circle, i.e. the current color selection */
+input[type="color" i]::-webkit-color-swatch {
+  border-radius: 50%;
+}
+
+input[type="color" i]::-moz-color-swatch {
+  border-radius: 50%;
+}
+</style>
