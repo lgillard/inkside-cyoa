db81e787883739d7ae6b9fb73739c65ae536b265
Lucille Gillard <gillard.lucille@gmail.com>
2025-03-20 12:35:37 +0100

    UPD: improve edit book view style

--- src/ui/components/MyButton.vue
+++ src/ui/components/MyButton.vue
@@ -6,11 +6,12 @@
 
 <style scoped lang="scss">
 button {
+  height: fit-content;
   display: inline-block;
   text-transform: uppercase;
   font-size: 1em;
   font-weight: 700;
-  padding: 5px 10px;
+  padding: 6px;
   border-radius: 5px;
   color: rgba(var(--v-theme-primary));
   border: rgba(var(--v-theme-primary)) solid 1px;
--- src/ui/components/_EditGamebookView/EditBookTitle.vue
+++ src/ui/components/_EditGamebookView/EditBookTitle.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="d-flex" v-if="!titleUpdInProgress">
-    <h2 class="text-primary text-h4 my-4">{{gamebookTitle}}</h2>
-    <button @click="() => titleUpdInProgress = true">
+    <h2 class="text-h4 my-4">{{gamebookTitle}}</h2>
+    <button id="edit-gamebook-title" class="btn" @click="() => titleUpdInProgress = true">
       <v-icon :icon="mdiLeadPencil" title="Modifier le titre du livre"/>
     </button>
   </div>
   <form class="d-flex" v-else>
     <input type="text" class="text-primary text-h4 my-4" v-model="gamebookTitle"/>
-    <button @click="saveTitle">
+    <MyButton class="btn" @click="saveTitle">
       <v-icon :icon="mdiCheck" title="Sauvegarder le titre du livre"/>
-    </button>
+    </MyButton>
   </form>
 </template>
 <script setup lang="ts">
@@ -17,6 +17,7 @@ import {ref} from "vue";
 import {mdiCheck, mdiLeadPencil} from "@mdi/js";
 import {useGamebookStore} from "@/application/stores/gamebook.store.ts";
 import {useRoute} from "vue-router";
+import MyButton from "@/ui/components/MyButton.vue";
 
 const route = useRoute();
 const gamebookStore = useGamebookStore();
@@ -32,3 +33,19 @@ function saveTitle() {
   ).then(() => { titleUpdInProgress.value = false });
 }
 </script>
+
+
+<style lang="scss">
+.btn {
+  margin-left: 12px;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+#edit-gamebook-title {
+  color: rgba(var(--v-theme-primary));
+
+  &:hover {
+    color: rgba(var(--v-theme-secondary));
+  }
+}
+</style>
