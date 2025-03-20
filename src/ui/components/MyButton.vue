470b4f1bf3edf0418ab03fb3a0b69c49ea168a31
Lucille Gillard <gillard.lucille@gmail.com>
2025-03-20 12:11:51 +0100

    ADD: create section button

---
+++ src/ui/components/MyButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>
+    <slot/>
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 1em;
+  font-weight: 700;
+  padding: 5px 10px;
+  border-radius: 5px;
+  color: rgba(var(--v-theme-primary));
+  border: rgba(var(--v-theme-primary)) solid 1px;
+}
+
+button:hover,
+button:focus {
+  color: rgba(var(--v-theme-secondary));
+  border-color: rgba(var(--v-theme-secondary));
+}
+</style>
--- src/ui/views/EditGamebookView.vue
+++ src/ui/views/EditGamebookView.vue
@@ -2,18 +2,25 @@
   <v-skeleton-loader v-if="gamebookStore.loading" type="card"/>
   <div v-else>
     <EditBookTitle/>
+    <MyButton @click="createSection">Nouvelle section</MyButton>
     <GamebookTree :gamebook="gamebookStore.currentGamebookTree"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import {useGamebookStore} from "@/application/stores/gamebook.store.ts";
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import GamebookTree from "@/ui/components/_EditGamebookView/GamebookTree.vue";
 import EditBookTitle from "@/ui/components/_EditGamebookView/EditBookTitle.vue";
+import MyButton from "@/ui/components/MyButton.vue";
 
 const route = useRoute();
+const router = useRouter();
 
 const gamebookStore = useGamebookStore();
 gamebookStore.fetchGamebookTree(route.params.gamebookId as string);
+
+function createSection() {
+  router.push(route.fullPath + '/section/new');
+}
 </script>
